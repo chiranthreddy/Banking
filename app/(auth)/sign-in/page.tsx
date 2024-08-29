09a50cb6075ca88dae9cf7ef42f530ed40bd9a26
chiranth <reddychiranth73@gmail.com>
2024-08-29 15:26:43 +0530

    initial file and folder structure

---
+++ app/(auth)/sign-in/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function SignIn() {
+  return (
+    <div>
+      <h1>Sign In Page</h1>
+      {/* Add your sign-in form or content here */}
+    </div>
+  );
+}
